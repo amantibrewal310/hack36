a29adc9f8b168e356b8ee6317a03bd907099739b
Aman <amantibrewal310@gmail.com>
2020-02-15 03:23:07 +0530

    Sign up url changes to register

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login/',views.login_view,name='login'),
-    path('signup/',views.signup_view,name='signup'),
+    path('register/',views.signup_view,name='signup'),
 
 ]
