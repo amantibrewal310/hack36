6513f3fe268dc57f31b43ba7168c1772c32a8e45
Aman <amantibrewal310@gmail.com>
2020-02-15 21:04:01 +0530

    url fixed

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     description = models.CharField(max_length=100, default="")
     city = models.CharField(max_length=100, default="")
     website = models.CharField(max_length=100, default="")
@@ -21,11 +21,11 @@ class UserProfile(models.Model):
         return "/accounts/profile/{}".format(self.pk)
 
 
-def create_profile(sender, **kwargs):
-    if kwargs['created']:
-        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+# def create_profile(sender, **kwargs):
+#     if kwargs['created']:
+#         user_profile = UserProfile.objects.create(user=kwargs['instance'])
 
-post_save.connect(create_profile, sender=User)
+# post_save.connect(create_profile, sender=User)
 
 
 class FriendRequest(models.Model):
