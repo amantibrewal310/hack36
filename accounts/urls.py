cf4fb9fd5c103919b1d591bb56e9dfacc274fb6d
harshjoeyit <harshit02gangwar@gmail.com>
2020-02-15 20:38:23 +0530

    templates corrected

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('sign_in/',views.sign_in_view,name='sign_in'),
     path('signout/',views.logout_view,name='logout'),
     path('profile/edit/',views.edit_profile,name='edit_profile'),
-    path('portfolio/edit',views.edit_portfolio,name='edit_portfolio')
+    path('portfolio/edit',views.edit_portfolio,name='edit_portfolio'),
 
     # temporary portfolio path created
     path('portfolio/', views.portfolio, name='portfolio'),
