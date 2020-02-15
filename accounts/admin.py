d9c9d4b042d4f244e90e9d131d811f6137ab1725
Ayush Srivastava <iayush.srivastava99@gmail.com>
2020-02-15 20:16:27 +0530

    solved conflict

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-
 from accounts.models import *
-
-from accounts.models import UserProfile, Skills
-
 
 # Register your models here.
 
@@ -11,5 +7,3 @@ admin.site.register(UserProfile)
 admin.site.register(FriendRequest)
 admin.site.register(Message)
 admin.site.register(Appoint)
-admin.site.register(Skills)
-
--- accounts/models.py
+++ accounts/models.py
@@ -4,28 +4,18 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 # Create your models here.
-class Skills(models.Model):
-	title = models.CharField(max_length = 30)
-	description = models.TextField()
-
-
-	def __str__(self):
-		return self.title
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
-    bio = models.TextField(default="")
-    image = models.ImageField(upload_to='profile/img/',blank=True,null=True)
-    address = models.CharField(max_length=80,blank=True,null=True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    description = models.CharField(max_length=100, default="")
     city = models.CharField(max_length=100, default="")
     website = models.CharField(max_length=100, default="")
     phone = models.IntegerField(default=0)
-    skills = models.TextField(null=True)
+    friends = models.ManyToManyField("UserProfile", blank=True)
 
     def __str__(self):
         return self.user.username
-
 
     def get_absolute_url(self):
         return "/accounts/profile/{}".format(self.pk)
@@ -66,11 +56,4 @@ class Appoint(models.Model):
 
     def __str__(self):
         return "From {}, to{}".format(self.from_user.username, self.to_user.username)
-
-
-# def create_profile(sender, **kwargs):
-#     if kwargs['created']:
-#         user_profile = UserProfile.objects.create(user=kwargs['instance'])
-
-# post_save.connect(create_profile, sender=User)
 
